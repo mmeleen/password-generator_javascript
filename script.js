e2007b783d370c2e2e2e91e7a5e38b5647ec7851
Matt Meleen <mmeleen@utexas.edu>
2020-06-18 00:16:52 -0500

    Shuffle function  implemented for password array

--- script.js
+++ script.js
@@ -88,7 +88,21 @@ function generatePassword(passLength, hasLow, hasUpp, hasNum, hasChar) {
 
   console.log(passArray);
 
+  var shuffledPass = shuffleArray(passArray);
+  console.log(shuffledPass);
 
+}
+
+// Randomize array in-place using Durstenfeld shuffle algorithm
+function shuffleArray(array) {
+  console.log(array);
+  for (var i = array.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+  }
+  return array;
 }
 
 // Add event listener to generate button
