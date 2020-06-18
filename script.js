a0dc1b286f3bea9255b5e0ba6c6c7fe459950d75
Matt Meleen <mmeleen@utexas.edu>
2020-06-17 23:29:26 -0500

    Added char sets and  boolean logic for each option

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//Password content options
+var numSet = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var lowSet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppSet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var charSet = ["!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", "/", ":", ";", ">", "?", "@", "[", "]", "^", "{", "}", "~", "|"];
+
+// Function for user prompts, passes values to writePassword
 function promptUser(){
   var length = prompt("Please type a password length (number between 8 and 128)");
   console.log(length);
@@ -31,8 +38,45 @@ function writePassword(length, hasLow, hasUpp, hasNum, hasChar) {
   passwordText.value = password;
 }
 
+//Generate password adhering to user criteria
 function generatePassword(length, hasLow, hasUpp, hasNum, hasChar) {
   //console.log(length, hasLow, hasUpp, hasNum, hasChar);
+  var totalSet = [];
+  var passArray = [];
+
+  if (hasLow) {
+    totalSet = totalSet.concat(lowSet);
+    console.log(totalSet);
+    var randomLow = lowSet[Math.floor(Math.random() * lowSet.length)];
+    passArray.push(randomLow);
+    console.log(passArray); 
+  }
+
+  if (hasUpp) {
+    totalSet = totalSet.concat(uppSet);
+    console.log(totalSet);
+    var randomUpp = UppSet[Math.floor(Math.random() * UppSet.length)];
+    passArray.push(randomUpp);
+    console.log(passArray);   
+  }
+
+  if (hasNum) {
+    totalSet = totalSet.concat(numSet);
+    console.log(totalSet);
+    var randomNum = NumSet[Math.floor(Math.random() * NumSet.length)];
+    passArray.push(randomNum);
+    console.log(passArray);   
+  }
+
+  if (hasChar) {
+    totalSet = totalSet.concat(charSet);
+    console.log(totalSet);
+    var randomChar = CharSet[Math.floor(Math.random() * CharSet.length)];
+    passArray.push(randomChar);
+    console.log(passArray);   
+  }
+
+
 }
 
 // Add event listener to generate button
